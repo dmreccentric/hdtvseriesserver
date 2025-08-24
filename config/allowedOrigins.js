6fbb84d0c7c1a4cd48b79a8790dad7bf6ad6ca90
dmreccentric <eccentricecc481@gmail.com>
2025-08-24 03:45:32 +0100

    added new domain to cors option

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000", // Next.js dev server
   "https://hdtvseriesapp.vercel.app", // vercel domain
+  "https://hdtvseries.xyz", // vercel domain
   "http://127.0.0.1:3000", // Sometimes used in dev
 ];
 
